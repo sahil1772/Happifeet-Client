1bd1af4587ef07f4d3a8908e31ef3fb91f7e74c2
sdbarve14 <sdbarve14@gmail.com>
2024-02-16 17:46:24 +0530

    loader addeded login page

--- lib/screens/Login/LoginPage.dart
+++ lib/screens/Login/LoginPage.dart
@@ -1,9 +1,11 @@
 import 'dart:developer';
+import 'dart:ui';
 
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:happifeet_client_app/model/Login/LoginData.dart';
 import 'package:happifeet_client_app/screens/Login/ForgotPassword.dart';
 import 'package:happifeet_client_app/utils/ColorParser.dart';
 import 'package:happifeet_client_app/utils/DeviceDimensions.dart';
@@ -12,6 +14,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import '../../components/BottomNavigation.dart';
 import '../../model/Login/UserData.dart';
 import '../../network/ApiFactory.dart';
+import '../../storage/runtime_storage.dart';
 import '../../storage/shared_preferences.dart';
 
 class LoginPageWidget extends StatefulWidget {
@@ -32,6 +35,7 @@ class _LoginPageWidgetState extends State<LoginPageWidget> {
   String? password;
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  Future<LoginData>? apiResposne;
 
   @override
   void initState() {
@@ -40,6 +44,15 @@ class _LoginPageWidgetState extends State<LoginPageWidget> {
 
     /** Display with status and navigation bar **/
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+  }
+
+  sendLoginDetails() async {
+    apiResposne = ApiFactory().getLoginService().sendLoginDetails(
+        "login", emailController.text, passwordController.text);
+
+    // var response = await apiResposne;
+
+    setState(() {});
   }
 
   setUserPermissions(UserData data) async {
@@ -65,155 +78,221 @@ class _LoginPageWidgetState extends State<LoginPageWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Stack(
-          children: [
-            Image.asset("assets/images/login/loginPNG.png",fit: BoxFit.fill,width: MediaQuery.of(context).size.width,),
-            // SvgPicture.asset("assets/images/login/loginBG.svg",fit: BoxFit.fill,width: MediaQuery.of(context).size.width,),
-            // SvgPicture.asset("assets/images/login/loginSVG.svg",fit: BoxFit.fill,width: MediaQuery.of(context).size.width,),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 22),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                // mainAxisAlignment: MainAxisAlignment.center,
+    try {
+      return Scaffold(
+        extendBodyBehindAppBar: true,
+        resizeToAvoidBottomInset: false,
+        body: SafeArea(
+          child: FutureBuilder<LoginData>(
+            future: apiResposne,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                log("RESPONSE LOGIN ${snapshot.data!.data}");
+                if (snapshot.data!.status == "1") {
+                  log("VALID USERNMAE IN LOGIN PAGE");
+                  SharedPref.instance
+                      .setAccessPermission(snapshot.data!.data!.access!);
+                  SharedPref.instance.setUserData(snapshot.data!.data!);
+                  SharedPref.instance
+                      .setLanguages(snapshot.data!.data!.language!);
+                  SharedPref.instance
+                      .saveTheme(snapshot.data!.data!.theme_data!);
+
+                  setUserPermissions(snapshot.data!.data!);
+                  Future.delayed(const Duration(milliseconds: 100),() {
+                    BottomNavigationHappiFeet().goToBottomNavigation(context);
+
+                  },);
+                } else {
+                  log("INVALID USERNAME IN LOGIN PAGE");
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(content: Text("Invalid username or password")));
+                }
+              }
+
+              return Stack(
                 children: [
-                  const SizedBox(
-                    height: 20,
+                  Image.asset(
+                    "assets/images/login/loginPNG.png",
+                    fit: BoxFit.fill,
+                    width: MediaQuery.of(context).size.width,
                   ),
-                  Center(child: Image.asset("assets/images/login/logo.png")),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                      controller: emailController,
-                      onTapOutside: (PointerDownEvent) {
-                        FocusManager.instance.primaryFocus?.unfocus();
-                      },
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        // labelText: labelText,
-                        hintText: 'Email',
-                        hintStyle: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w400),
-                        // errorText: getEmailError(),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: BorderSide(
-                                color: ColorParser().hexToColor("#D7D7D7"),
-                                width: 1)),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: BorderSide(
-                                width: 1,
-                                color: ColorParser().hexToColor("#D7D7D7"))),
-                      )),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  TextField(
-                      controller: passwordController,
-                      onTapOutside: (PointerDownEvent) {
-                        FocusManager.instance.primaryFocus?.unfocus();
-                      },
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.white,
-                        // labelText: labelText,
+                  // SvgPicture.asset("assets/images/login/loginBG.svg",fit: BoxFit.fill,width: MediaQuery.of(context).size.width,),
+                  // SvgPicture.asset("assets/images/login/loginSVG.svg",fit: BoxFit.fill,width: MediaQuery.of(context).size.width,),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 22),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      // mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        Center(
+                            child: Image.asset("assets/images/login/logo.png")),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                            controller: emailController,
+                            onTapOutside: (PointerDownEvent) {
+                              FocusManager.instance.primaryFocus?.unfocus();
+                            },
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              // labelText: labelText,
+                              hintText: 'Email',
+                              hintStyle: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w400),
+                              // errorText: getEmailError(),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(
+                                      color:
+                                          ColorParser().hexToColor("#D7D7D7"),
+                                      width: 1)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(
+                                      width: 1,
+                                      color:
+                                          ColorParser().hexToColor("#D7D7D7"))),
+                            )),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        TextField(
+                            controller: passwordController,
+                            onTapOutside: (PointerDownEvent) {
+                              FocusManager.instance.primaryFocus?.unfocus();
+                            },
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.white,
+                              // labelText: labelText,
 
-                        hintText: 'Password',
-                        hintStyle: const TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.w400),
-                        // errorText: getEmailError(),
-                        focusedBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: BorderSide(
-                                color: ColorParser().hexToColor("#D7D7D7"),
-                                width: 1)),
-                        enabledBorder: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(12),
-                            borderSide: BorderSide(
-                                width: 1,
-                                color: ColorParser().hexToColor("#D7D7D7"))),
-                      )),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      InkWell(
-                          onTap: () {
-                            const ForgotPasswordWidget()
-                                .goToForgotPasswordPage(context);
+                              hintText: 'Password',
+                              hintStyle: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w400),
+                              // errorText: getEmailError(),
+                              focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(
+                                      color:
+                                          ColorParser().hexToColor("#D7D7D7"),
+                                      width: 1)),
+                              enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                  borderSide: BorderSide(
+                                      width: 1,
+                                      color:
+                                          ColorParser().hexToColor("#D7D7D7"))),
+                            )),
+                        const SizedBox(
+                          height: 15,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            InkWell(
+                                onTap: () {
+                                  const ForgotPasswordWidget()
+                                      .goToForgotPasswordPage(context);
+                                },
+                                child: const Text(
+                                  "Forgot Password",
+                                  style: TextStyle(
+                                      color: Colors.red, fontSize: 14),
+                                )),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            fixedSize: const Size(320, 51),
+                            backgroundColor:
+                                ColorParser().hexToColor("#01825C"),
+                          ),
+                          onPressed: () async {
+                            sendLoginDetails();
+                            /**  **/
+                            // var response = await ApiFactory()
+                            //     .getLoginService()
+                            //     .sendLoginDetails("login", emailController.text,
+                            //         passwordController.text);
+                            //
+                            // log("RESPONSE LOGIN ${response.data}");
+                            // if (response.status == "1") {
+                            //   log("VALID USERNMAE IN LOGIN PAGE");
+                            //   SharedPref.instance
+                            //       .setAccessPermission(response.data!.access!);
+                            //   SharedPref.instance.setUserData(response.data!);
+                            //   SharedPref.instance
+                            //       .setLanguages(response.data!.language!);
+                            //   SharedPref.instance
+                            //       .saveTheme(response.data!.theme_data!);
+                            //
+                            //   setUserPermissions(response.data!);
+                            //   BottomNavigationHappiFeet()
+                            //       .goToBottomNavigation(context);
+                            // } else {
+                            //   log("INVALID USERNAME IN LOGIN PAGE");
+                            //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            //       content: Text("Invalid username or password")));
+                            // }
+                            // // SharedPref.instance.setPermissions();
+                            //
+                            // // OtpPageWidget().goToOtpPage(context);
                           },
-                          child: const Text(
-                            "Forgot Password",
-                            style: TextStyle(color: Colors.red, fontSize: 14),
-                          )),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(320, 51),
-                      backgroundColor: ColorParser().hexToColor("#01825C"),
-
-                      // shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      //     RoundedRectangleBorder(
-                      //         borderRadius: BorderRadius.circular(12.0),
-                      //         // side: BorderSide(color: Colors.red)
-                      //     )
-                      // ),
+                          child: const Text("Login",
+                              style: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: Colors.white)),
+                        )
+                        // SvgPicture.asset("assets/images/login/logo.svg"),
+                      ],
                     ),
-                    onPressed: () async {
-                      var response = await ApiFactory()
-                          .getLoginService()
-                          .sendLoginDetails("login", emailController.text,
-                              passwordController.text);
+                  ),
 
-                      log("RESPONSE LOGIN ${response.data}");
-                      if (response.status == "1") {
-                        log("VALID USERNMAE IN LOGIN PAGE");
-                        SharedPref.instance
-                            .setAccessPermission(response.data!.access!);
-                        SharedPref.instance.setUserData(response.data!);
-                        SharedPref.instance
-                            .setLanguages(response.data!.language!);
-                        SharedPref.instance
-                            .saveTheme(response.data!.theme_data!);
-
-                        setUserPermissions(response.data!);
-                        BottomNavigationHappiFeet()
-                            .goToBottomNavigation(context);
-                      } else {
-                        log("INVALID USERNAME IN LOGIN PAGE");
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            content: Text("Invalid username or password")));
-                      }
-                      // SharedPref.instance.setPermissions();
-
-                      // OtpPageWidget().goToOtpPage(context);
-                    },
-                    child: const Text("Login",
-                        style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.white)),
-                  )
-                  // SvgPicture.asset("assets/images/login/logo.svg"),
+                  snapshot.connectionState == ConnectionState.waiting
+                      ? Center(
+                          child: ClipRect(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(
+                                sigmaX: 5.0,
+                                sigmaY: 5.0,
+                              ),
+                              child: Container(
+                                alignment: Alignment.center,
+                                child: AbsorbPointer(
+                                  absorbing: true,
+                                  child: Center(
+                                    child: CircularProgressIndicator(
+                                        color: Theme.of(context).primaryColor),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      : const SizedBox()
                 ],
-              ),
-            ),
-          ],
+              );
+            },
+          ),
         ),
-      ),
-    );
+      );
+    } catch (e) {
+      return Scaffold(
+        body: Center(
+          child: Text("$e"),
+        ),
+      );
+    }
   }
 }
